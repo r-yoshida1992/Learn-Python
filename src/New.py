1ad537724ee78f48ab95a00507be5c0acf2219d5
r-yoshida1992 <yoshida@link-ps.co.jp>
2021-07-09 00:20:25 +0900

    ちょこっとリファクタ

--- src/New.py
+++ src/New.py
@@ -2,17 +2,22 @@
 # --使い方--
 # ・「New.py hoge」で実行
 # ・hoge.xlsxがカレントディレクトリに作成される。
-# ・既に同名ファイルが存在していた場合上書きしてしまうので、注意 TODO ←上書きしないように修正する。
+# ・既に同名ファイルが存在していた場合、ファイルは作成されない。
 
 import sys
 import openpyxl
+import os
 
 # コマンドラインで引数が渡されているかを検査
 if len(sys.argv) > 1:
-    wb = openpyxl.Workbook()
     fileName = sys.argv[1] + '.xlsx'
-    wb.save(fileName)
-    print(fileName + 'を作成しました。')
+    # 既に同名ファイルが存在していた場合、ファイルは作成されない。
+    if os.path.exists(fileName):
+        print('既に[' + fileName + ']は存在している為、作成できませんでした。')
+    else:
+        wb = openpyxl.Workbook()
+        wb.save(fileName)
+        print(fileName + 'を作成しました。')
 else:
     # 引数の指定がない
     print('ファイル名を指定して下さい。')
--- src/SheetList.py
+++ src/SheetList.py
@@ -1,13 +1,14 @@
-# カレントディレクトリのエクセルファイルからシート名を全て出力する
+# 引数で指定したディレクトリ配下のエクセルファイルからシート名を全て出力する
 # --使い方--
 # ・「SheetList.py」で実行
-# ・カレントディレクトリに存在するxlsxおよびxlsmファイルから、シート名を出力する。
+# ・指定のディレクトリに存在するxlsxおよびxlsmファイルから、シート名を出力する。
 
 import glob
 import openpyxl
+import sys
 
-# カレントディレクトリのファイル一覧を取得
-files = glob.glob("*")
+# [真の場合] if [条件式] else [偽の場合]
+files = [glob.glob(sys.argv[1] + "\*") if len(sys.argv) > 1 else glob.glob("*")]
 
 for file in files:
     # xlsとxlsbはopenpyxlでは対応していない
